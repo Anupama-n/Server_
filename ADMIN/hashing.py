1c0acfac382479186dec28b9c3c1fba80c7efab9
Anupama-n <anupaneupane1994@gmail.com>
2024-06-19 09:30:24 +0545

    created admin create api

--- ADMIN/hashing.py
+++ ADMIN/hashing.py
@@ -5,3 +5,6 @@ bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 def get_password_hash(password: str) -> str:
     return bcrypt_context.hash(password)
+
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    return bcrypt_context.verify(plain_password, hashed_password)
--- ADMIN/routers/auth.py
+++ ADMIN/routers/auth.py
@@ -1,11 +1,29 @@
 from fastapi import FastAPI, Depends, HTTPException, APIRouter
 from sqlalchemy.orm import Session
+from rest_framework import status
 from schemas import *
 from models import *
 from database import *
+from hashing import *
 
 app = FastAPI()
 router = APIRouter()
+
+@router.post("/admin/create", status_code=status.HTTP_201_CREATED)
+def create_admin(admin: Admin, db: Session = Depends(get_db_admin)):
+    user = db.query(AdminModel).filter(AdminModel.username == admin.username).first()
+
+    if user:
+        raise HTTPException(status_code=400, detail="Username already exists")
+
+    hashed_password = get_password_hash(admin.password)
+    new_admin = AdminModel(username=admin.username, password=hashed_password)
+
+    db.add(new_admin)
+    db.commit()
+    db.refresh(new_admin)
+
+    return {"message": "Admin created successfully"}
 
 @router.post("/admin")
 def admin_login(admin: Admin, db: Session = Depends(get_db_admin)):
